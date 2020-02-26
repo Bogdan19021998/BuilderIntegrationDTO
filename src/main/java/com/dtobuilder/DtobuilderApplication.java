f95933f33fd059892620518591c13eb48ad8a4c0
Bogdan <bogdan19021998ua@gmail.com>
2020-02-26 21:18:55 +0200

    Created empty project

---
+++ src/main/java/com/dtobuilder/DtobuilderApplication.java
@@ -0,0 +1,13 @@
+package com.dtobuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtobuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtobuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dtobuilder/DtobuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtobuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtobuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
